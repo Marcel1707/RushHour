9c92ad2361eed6a7e9a1705f0dbaa4873e1c6f61
Marcel Ciesla <S2110455024@students.fh-hagenberg.at>
2022-05-02 13:12:48 +0200

    add main class with statistics

--- src/at/fhooe/ai/rushhour/RushHour.java
+++ src/at/fhooe/ai/rushhour/RushHour.java
@@ -1,6 +1,11 @@
 package at.fhooe.ai.rushhour;
-import java.io.*;
-import java.text.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 /**
  * This class contains only a simple main for testing your algorithm on data
@@ -15,118 +20,141 @@ import java.text.*;
  */
 public class RushHour {
 
-  /**
-   * The main for running all puzzles with all heuristics, and printing out all
-   * solution paths as well as a final summary table.
-   */
-  public static void main(String argv[]) throws FileNotFoundException, IOException {
+    /**
+     * The main for running all puzzles with all heuristics, and printing out all
+     * solution paths as well as a final summary table.
+     */
+    public static void main(String argv[]) throws FileNotFoundException, IOException {
+        // read all the puzzles in file named by first argument
+        Puzzle[] puzzles = Puzzle.readPuzzlesFromFile(argv[0]);
 
-    // read all the puzzles in file named by first argument
-    Puzzle[] puzzles = Puzzle.readPuzzlesFromFile(argv[0]);
+        String[] heuristic_names = null;
+        int num_puzzles = puzzles.length;
+        int num_heuristics = 0;
 
-    String[] heuristic_names = null;
-    int num_puzzles = puzzles.length;
-    int num_heuristics = 0;
+        int[][] num_expanded = null;
+        int[][] soln_depth = null;
 
-    int[][] num_expanded = null;
-    int[][] soln_depth = null;
+        // run each heuristic on each puzzle
+        for (int i = 0; i < num_puzzles; i++) {
+            System.out.println("=================================================");
+            System.out.println("puzzle = " + puzzles[i].getName());
 
-    // run each heuristic on each puzzle
-    for (int i = 0; i < num_puzzles; i++) {
-      System.out.println("=================================================");
-      System.out.println("puzzle = " + puzzles[i].getName());
+            Heuristic[] heuristics = { // these are the heuristics to be used
+                    new ZeroHeuristic(puzzles[i]), new BlockingHeuristic(puzzles[i]), new AdvancedHeuristic(puzzles[i])};
 
-      /*Heuristic[] heuristics = { // these are the heuristics to be used
-          new ZeroHeuristic(puzzles[i]), new BlockingHeuristic(puzzles[i]), new AdvancedHeuristic(puzzles[i]), };*/
+            if (i == 0) {
+                num_heuristics = heuristics.length;
+                num_expanded = new int[num_puzzles][num_heuristics];
+                soln_depth = new int[num_puzzles][num_heuristics];
 
-      Heuristic[] heuristics = { new ZeroHeuristic(puzzles[i]), new BlockingHeuristic(puzzles[i]) };
+                heuristic_names = new String[num_heuristics];
+                for (int h = 0; h < num_heuristics; h++)
+                    heuristic_names[h] = heuristics[h].getClass().getName();
+            }
 
-      if (i == 0) {
-        num_heuristics = heuristics.length;
-        num_expanded = new int[num_puzzles][num_heuristics];
-        soln_depth = new int[num_puzzles][num_heuristics];
+            for (int h = 0; h < num_heuristics; h++) {
+                System.out.println();
+                System.out.println("------------------------------------");
+                System.out.println();
+                System.out.println("heuristic = " + heuristic_names[h]);
 
-        heuristic_names = new String[num_heuristics];
+                puzzles[i].resetSearchCount();
+                AStar search = new AStar(puzzles[i], heuristics[h]);
+
+                if (search.path == null) {
+                    System.out.println("NO SOLUTION FOUND.");
+                    soln_depth[i][h] = -1;
+                } else {
+
+                    for (int j = 0; j < search.path.length; j++) {
+                        search.path[j].print();
+                        System.out.println();
+                    }
+
+                    num_expanded[i][h] = puzzles[i].getSearchCount();
+                    soln_depth[i][h] = search.path.length - 1;
+
+                    System.out.println("nodes expanded: " + num_expanded[i][h] + ", soln depth: " + soln_depth[i][h]);
+
+                }
+            }
+        }
+
+        // print the results in a table
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        System.out.print("          ");
         for (int h = 0; h < num_heuristics; h++)
-          heuristic_names[h] = heuristics[h].getClass().getName();
-      }
-
-      for (int h = 0; h < num_heuristics; h++) {
+            System.out.print(" |    " + right_pad(heuristic_names[h], 18));
         System.out.println();
-        System.out.println("------------------------------------");
+
+        System.out.print("name      ");
+        for (int h = 0; h < num_heuristics; h++)
+            System.out.print(" |    nodes dpth  br.fac");
         System.out.println();
-        System.out.println("heuristic = " + heuristic_names[h]);
 
-        puzzles[i].resetSearchCount();
-        AStar search = new AStar(puzzles[i], heuristics[h]);
+        System.out.print("----------");
+        for (int h = 0; h < num_heuristics; h++)
+            System.out.print("-+----------------------");
+        System.out.println();
 
-        if (search.path == null) {
-          System.out.println("NO SOLUTION FOUND.");
-          soln_depth[i][h] = -1;
-        } else {
+        NumberFormat brfac_nf = new DecimalFormat("##0.000");
 
-          for (int j = 0; j < search.path.length; j++) {
-            search.path[j].print();
+        for (int i = 0; i < num_puzzles; i++) {
+            System.out.print(right_pad(puzzles[i].getName(), 10));
+
+            for (int h = 0; h < num_heuristics; h++) {
+                if (soln_depth[i][h] < 0) {
+                    System.out.print(" |  ** search failed ** ");
+                } else {
+                    System.out.print(" | " + left_pad(Integer.toString(num_expanded[i][h]), 8) + " "
+                            + left_pad(Integer.toString(soln_depth[i][h]), 4) + " "
+                            + left_pad(brfac_nf.format(BranchingFactor.compute(num_expanded[i][h], soln_depth[i][h])), 7));
+                }
+            }
             System.out.println();
-          }
-
-          num_expanded[i][h] = puzzles[i].getSearchCount();
-          soln_depth[i][h] = search.path.length - 1;
-
-          System.out.println("nodes expanded: " + num_expanded[i][h] + ", soln depth: " + soln_depth[i][h]);
-
         }
-      }
+
+        System.out.println();
+
+        float[][] relativeImprovements = new float[num_puzzles][num_heuristics];
+
+        for (int i = 0; i < num_puzzles; i++) {
+            int expandedZeroHeuristic = num_expanded[i][0];
+
+            for (int j = 0; j < num_heuristics; j++) {
+                int diff = expandedZeroHeuristic - num_expanded[i][j];
+                relativeImprovements[i][j] = diff * 1.0f / expandedZeroHeuristic;
+            }
+        }
+
+        for (int j = 0; j < num_heuristics; j++) {
+            float improvementSum = 0;
+
+            for (int i = 0; i < num_puzzles; i++) {
+                improvementSum += relativeImprovements[i][j];
+            }
+
+            BigDecimal mean = BigDecimal.valueOf(improvementSum / num_puzzles * 100d).setScale(1, RoundingMode.HALF_UP);
+
+            System.out.printf("heuristic %d relative improvement in expanded nodes (to zero heuristic): %s%%\n", j, mean);
+        }
+
     }
 
-    // print the results in a table
-    System.out.println();
-    System.out.println();
-    System.out.println();
-
-    System.out.print("          ");
-    for (int h = 0; h < num_heuristics; h++)
-      System.out.print(" |    " + right_pad(heuristic_names[h], 18));
-    System.out.println();
-
-    System.out.print("name      ");
-    for (int h = 0; h < num_heuristics; h++)
-      System.out.print(" |    nodes dpth  br.fac");
-    System.out.println();
-
-    System.out.print("----------");
-    for (int h = 0; h < num_heuristics; h++)
-      System.out.print("-+----------------------");
-    System.out.println();
-
-    NumberFormat brfac_nf = new DecimalFormat("##0.000");
-
-    for (int i = 0; i < num_puzzles; i++) {
-      System.out.print(right_pad(puzzles[i].getName(), 10));
-
-      for (int h = 0; h < num_heuristics; h++) {
-        if (soln_depth[i][h] < 0) {
-          System.out.print(" |  ** search failed ** ");
-        } else {
-          System.out.print(" | " + left_pad(Integer.toString(num_expanded[i][h]), 8) + " "
-              + left_pad(Integer.toString(soln_depth[i][h]), 4) + " "
-              + left_pad(brfac_nf.format(BranchingFactor.compute(num_expanded[i][h], soln_depth[i][h])), 7));
-        }
-      }
-      System.out.println();
+    private static String left_pad(String s, int n) {
+        for (int i = n - s.length(); i > 0; i--)
+            s = " " + s;
+        return s;
     }
-  }
 
-  private static String left_pad(String s, int n) {
-    for (int i = n - s.length(); i > 0; i--)
-      s = " " + s;
-    return s;
-  }
-
-  private static String right_pad(String s, int n) {
-    for (int i = n - s.length(); i > 0; i--)
-      s = s + " ";
-    return s.substring(0, n);
-  }
+    private static String right_pad(String s, int n) {
+        for (int i = n - s.length(); i > 0; i--)
+            s = s + " ";
+        return s.substring(0, n);
+    }
 
 }
